45b1ff3d635b7396677b8577c6622bca5727e142
Laureline Vaucoy <laureline.vaucoy@gmail.com>
2021-03-30 19:19:35 +0100

    File routes working

--- src/main/java/com/example/boring_folders_lab/components/DataLoader.java
+++ src/main/java/com/example/boring_folders_lab/components/DataLoader.java
@@ -1,7 +1,9 @@
 package com.example.boring_folders_lab.components;
 
+import com.example.boring_folders_lab.models.File;
 import com.example.boring_folders_lab.models.Folder;
 import com.example.boring_folders_lab.models.User;
+import com.example.boring_folders_lab.repositories.FileRepository;
 import com.example.boring_folders_lab.repositories.FolderRepository;
 import com.example.boring_folders_lab.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,6 +20,9 @@ public class DataLoader implements ApplicationRunner {
     @Autowired
     FolderRepository folderRepository;
 
+    @Autowired
+    FileRepository fileRepository;
+
     public DataLoader() {
 
     }
@@ -25,8 +30,14 @@ public class DataLoader implements ApplicationRunner {
     public void run(ApplicationArguments args) {
         User user1 = new User("Bob");
         userRepository.save(user1);
+
         Folder folder1 = new Folder("Shame Folder", user1);
         folderRepository.save(folder1);
+
+        File file1 = new File(folder1, "shamefulFile", ".mp4", 200);
+        File file2 = new File(folder1, "anotherShamefulFile", ".md", 1);
+        fileRepository.save(file1);
+        fileRepository.save(file2);
     }
 
 }
